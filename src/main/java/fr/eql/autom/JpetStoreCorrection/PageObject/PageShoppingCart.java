b6521e7e7e8238b4a6a246ad387d5c4f1c63ca31
unknown <kme75013@gmail.com>
2021-03-25 15:08:10 +0100

    Modification quantité

--- src/main/java/fr/eql/autom/JpetStoreCorrection/PageObject/PageShoppingCart.java
+++ src/main/java/fr/eql/autom/JpetStoreCorrection/PageObject/PageShoppingCart.java
@@ -6,9 +6,21 @@ import org.openqa.selenium.support.FindBy;
 public class PageShoppingCart extends BandeauMenu {
 	@FindBy(tagName = "h2")
 	private WebElement title;
-
+	
+	@FindBy (name = "EST-1")
+	WebElement quantityField;
+	
+	
+	public void ModifyQuantity(String quantity) {
+		quantityField.clear();
+		quantityField.sendKeys(quantity);
+	}
+	
+ 
 	public WebElement getTitle() {
 		return title;
+		
+		
 	}
 
 }
